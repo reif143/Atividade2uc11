4be2ed19ac46a6ca8150bc4dfee1fe5766b467c0
rafael <rafaelreifs23@hotmail.com>
2024-03-16 10:06:02 -0300

    definição de qual tela abrir primeiro, opção tela CadastroVIEW

--- LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/src/ProdutosDAO.java
@@ -5,21 +5,29 @@ import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 
-public class ProdutosDAO {
 
+public class ProdutosDAO {
+    
     Connection conn;
     PreparedStatement prep;
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-
-    public void cadastrarProduto(ProdutosDTO produto) {
-
+    
+    public void cadastrarProduto (ProdutosDTO produto){
+        
+        
         //conn = new conectaDAO().connectDB();
+        
+        
     }
-
-    public ArrayList<ProdutosDTO> listarProdutos() {
-
+    
+    public ArrayList<ProdutosDTO> listarProdutos(){
+        
         return listagem;
     }
-
+    
+    
+    
+        
 }
+
