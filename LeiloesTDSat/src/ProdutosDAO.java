92732e3aaef5e1d11ea767b91d9e37ef1cdb2e14
rafael <rafaelreifs23@hotmail.com>
2024-03-16 10:42:43 -0300

    implementação dos métodos vender e listar ProdutosDAO

--- LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/src/ProdutosDAO.java
@@ -78,5 +78,67 @@ public class ProdutosDAO {
         }
         return listagem;
     }
+
+    public void venderProduto(int idProduto) {
+        try {
+            conn = new conectaDAO().connectDB();
+            String sql = "UPDATE produtos SET status = 'Vendido' WHERE id = ?";
+            prep = conn.prepareStatement(sql);
+            prep.setInt(1, idProduto);
+            int rowsAffected = prep.executeUpdate();
+            if (rowsAffected > 0) {
+                JOptionPane.showMessageDialog(null, "Parabéns!!! produto vendido com sucesso!");
+            } else {
+                JOptionPane.showMessageDialog(null, "Que pena!!! falha ao vender este produto!");
+            }
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Opss!!! Erro ao vender produto: " + erro.getMessage());
+        } finally {
+            closeResources();
+        }
+    }
+
+    public ArrayList<ProdutosDTO> listarProdutosVendidos() {
+        ArrayList<ProdutosDTO> listagemVendidos = new ArrayList<>();
+        try {
+            conn = new conectaDAO().connectDB();
+            String sql = "SELECT * FROM produtos WHERE status = 'Vendido'";
+            prep = conn.prepareStatement(sql);
+            resultset = prep.executeQuery();
+            while (resultset.next()) {
+                ProdutosDTO produto = new ProdutosDTO();
+                produto.setId(resultset.getInt("id"));
+                produto.setNome(resultset.getString("nome"));
+                produto.setValor(resultset.getInt("valor"));
+                produto.setStatus(resultset.getString("status"));
+                listagemVendidos.add(produto);
+            }
+
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Opa!!! Erro ao listar os produtos vendidos: " + erro.getMessage());
+        } finally {
+            closeResources();
+        }
+        return listagemVendidos;
+    }
+
+    //Metodo auxiliar para fechar os recursos
+    private void closeResources() {
+        try {
+            if (resultset != null) {
+                resultset.close();
+            }
+            if (prep != null) {
+                prep.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
 
