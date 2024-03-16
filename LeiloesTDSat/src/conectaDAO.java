6c9e0460c496a6270e901fb8d0f825e5bfbaeee3
rafael <rafaelreifs23@hotmail.com>
2024-03-16 09:57:03 -0300

    adição e configuração do banco de dados

--- LeiloesTDSat/src/conectaDAO.java
+++ LeiloesTDSat/src/conectaDAO.java
@@ -11,7 +11,7 @@ public class conectaDAO {
 
         try {
 
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=reif");
 
         } catch (SQLException erro) {
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
